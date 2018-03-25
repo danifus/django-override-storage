be37af5195d6afbe20de139c65306bcbbd85864c
Daniel Hillier <daniel.hillier@gmail.com>
2018-03-25 22:02:08 +1100

    Update setup.py to simplify release process

--- setup.py
+++ setup.py
@@ -1,24 +1,102 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'django-override-storage'
+DESCRIPTION = 'Django test helpers to manage file storage side effects.'
+URL = 'https://github.com/danifus/django-override-storage'
+EMAIL = 'daniel.hillier@gmail.com'
+AUTHOR = 'Daniel Hillier'
+REQUIRES_PYTHON = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*'
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'django',
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, 'override_storage', '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 
-with open('README.rst') as f:
-    readme = f.read()
+class UploadCommand(Command):
+    """Support setup.py upload."""
 
-with open('LICENSE') as f:
-    license = f.read()
+    description = 'Build and publish the package.'
+    user_options = []
 
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name='django-override-storage',
-    version='0.1.2',
-    description='Django test helpers to manage file storage side effects.',
-    long_description=readme,
-    author='Daniel Hillier',
-    author_email='daniel.hillier@gmail.com',
-    url='https://github.com/danifus/django-override-storage',
-    license=license,
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
     packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -38,5 +116,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ]
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
